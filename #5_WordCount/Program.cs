22bac19f58179eca05747ffca3b60fa8f590611a
cemal sezer <cemalszr35@gmail.com>
2022-01-24 22:02:08 +0300

    WordCount Added

---
+++ #5_WordCount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5_WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int character = 0;
+             Console.Write("Please enter to sentence: ");
+             string answer = Console.ReadLine();
+             string[] words = answer.Split(" ");
+             Console.WriteLine($" Words Count: {words.Length}");
+             for (var i = 0; i < words.Length; i++)
+             {
+                 char[] characterZone = words[i].ToCharArray();
+                 character+=characterZone.Length;
+             }
+            Console.WriteLine($"Character Count: {character}");
+            
+          
+        }
+    }
+}
