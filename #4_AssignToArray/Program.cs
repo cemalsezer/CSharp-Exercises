6a9d200a597835a39e4a05b0289ecc5385b76a0d
cemal sezer <cemalszr35@gmail.com>
2022-01-24 21:35:11 +0300

    Algorithm is done

---
+++ #4_AssignToArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _4_AssignToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.Write("Please enter to loop's num: ");
+            int loopNum = int.Parse(Console.ReadLine());
+            string[] answer = new string[loopNum];
+             
+                for (int j = 0; j < answer.Length; j++)
+                {
+                    Console.Write("{0}: Please enter to word : ",j+1);
+                    answer[j] = Console.ReadLine();
+                }
+                Array.Reverse(answer);
+                foreach (var item in answer)
+                {
+                    Console.WriteLine(item);
+                }
+        }
+    }
+}
