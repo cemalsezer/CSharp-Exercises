34652d46d3b1da232ccf31510443daae1d1eb527
cemal sezer <cemalszr35@gmail.com>
2022-01-26 13:11:30 +0300

    Example for ctor

---
+++ #10_SimpleMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10_SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Example for ctor
+            int id; string name, surName;
+            Console.Write("Id: ");
+            id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+             Console.Write("Surname: ");
+            surName = Console.ReadLine();
+
+            studentRegister stnd =new studentRegister(id,name,surName);
+
+            Console.Write($"Id NO: {id}"+" ");
+            Console.Write($"Name: {name}"+" ");
+            Console.Write($"Surname: {surName}"+" ");
+
+            
+
+
+        }
+    }
+}
